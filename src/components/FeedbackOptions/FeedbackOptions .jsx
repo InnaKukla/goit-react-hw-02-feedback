818dcf60dc7f40907fc280d4e487b53e04acf2db
Inna Kukla <0969125133@ukr.net>
2023-02-21 19:15:56 +0200

    Create FeedbackOptions .jsx

---
+++ src/components/FeedbackOptions/FeedbackOptions .jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import { ButtonFeedback, ButtonWrapper } from './FeedbackOptions.styled';
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ButtonWrapper>
+      {options.map(option => {
+        return (
+          <div key={option}>
+            <ButtonFeedback
+              type="button"
+              name={option}
+              onClick={onLeaveFeedback}
+            >
+              {option}
+            </ButtonFeedback>
+          </div>
+        );
+      })}
+    </ButtonWrapper>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array,
+  onLeaveFeedback: PropTypes.func,
+};
