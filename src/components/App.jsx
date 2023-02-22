7dd24ab592996cbe15c2cac82e6c014ec9dd835f
Inna Kukla <0969125133@ukr.net>
2023-02-22 22:56:09 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { Component } from 'react';
-import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions ';
-import { Statistics } from './Statistics/Statistics';
-import { Section } from './Section/Section';
-import { Notification } from './Notification/Notification';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Statistics } from './Statistics';
+import { Section } from './Section';
+import { Notification } from './Notification';
 
 export class App extends Component {
   state = {
